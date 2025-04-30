f4f14660c22aac2d0077fd1fc097e7679f969845
SiAn20 <sidneyangellysg@gmail.com>
2025-04-30 14:41:23 -0400

    feat(SeatCounter): Implement component in Reserve Screen

--- Client/src/components/SeatCounter.jsx
+++ Client/src/components/SeatCounter.jsx
@@ -1,29 +1,62 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
 
 export const SeatCounter = () => {
   return (
-    <View style={Style.container}>
-      <Text style={Style.text}>holiii john</Text>
+    <View style={styles.container}>
+      <Text style={styles.label}>Número de asientos</Text>
+      <View style={styles.seatContainer}>
+        <Pressable style={styles.pressable}>
+          <Text style={styles.textPressable}>-</Text>
+        </Pressable>
+        <Text style={styles.text}>1</Text>
+        <Pressable style={styles.pressable}>
+          <Text style={styles.textPressable}>+</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
 
-const Style = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F8F9FF",
-    borderWidth: 1,
-    borderColor: "#E6E8FF",
-    borderRadius: 15,
-    width: "100%",
-    height: 100,
-    padding: 10,
     marginBottom: 20,
   },
+  label: {
+    color: "#4318D1",
+    fontWeight: "bold",
+    marginBottom: 8,
+  },
+  seatContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    borderWidth: 1,
+    borderColor: "#E6E8FF",
+    borderRadius: 20,
+    backgroundColor: "#F8F9FF",
+    height: 60,
+  },
+  pressable: {
+    width: 40,
+    height: 40,
+    borderRadius: 15,
+    backgroundColor: "#FFF",
+    alignItems: "center",
+    justifyContent: "center",
+    marginHorizontal: 25,
+    borderWidth: 1,
+    borderColor: "#E6E8FF",
+    elevation: 1.5,
+  },
+  textPressable: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#4318D1",
+  },
   text: {
-    fontSize: 25,
+    fontSize: 20,
     fontWeight: "bold",
   },
 });
