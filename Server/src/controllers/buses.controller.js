6c81e64923e2baf0b09dceecf2993f27bd4ed89e
RODRIGOVR <RodrigoVR70@gmail.com>
2025-05-16 14:12:39 -0400

    Update: Buses latitud longitud

--- Server/src/controllers/buses.controller.js
+++ Server/src/controllers/buses.controller.js
@@ -32,8 +32,8 @@ export const getBuses = (req, res) => {
                        "direccion": "Plaza Sucre, Av. 6 de Agosto",
                        "telefono": "N/D",
                        "coordenadas": {
-                         "latitud": null,
-                         "longitud": null
+                         "latitud": -16.16558576888352
+                         "longitud": -69.08817443058417
                        },
                        "notas": "Parada principal en Copacabana (localidad turística a orillas del lago Titicaca)."
                      },
@@ -44,20 +44,20 @@ export const getBuses = (req, res) => {
                        "direccion": "Zona frontera (Desaguadero)",
                        "telefono": "N/D",
                        "coordenadas": {
-                         "latitud": null,
-                         "longitud": null
+                         "latitud": -16.562889470300362
+                         "longitud": -69.04477187213917
                        },
                        "notas": "Parada fronteriza en la localidad de Desaguadero (frontera con Perú)."
                      },
                      {
-                       "nombre": "Parada de buses de Tiwanaku",
+                       "nombre": "Terminal de buses Pachakuti",
                        "ciudad": "Tiwanaku",
                        "departamento": "La Paz",
                        "direccion": "Plaza principal de Tiwanaku",
                        "telefono": "N/D",
                        "coordenadas": {
-                         "latitud": null,
-                         "longitud": null
+                         "latitud": -16.55251022524711,
+                         "longitud": -68.68190419768453
                        },
                        "notas": "Parada utilizada por buses turísticos y locales en Tiwanaku (sitio arqueológico)."
                      }
@@ -87,29 +87,17 @@ export const getBuses = (req, res) => {
                        },
                        "notas": "Antigua terminal de Oruro (1982-2017), actualmente utilizada para buses interprovinciales/locales."
                      },
-                     {
-                       "nombre": "Parada de buses de Pisiga",
-                       "ciudad": "Pisiga",
-                       "departamento": "Oruro",
-                       "direccion": "Recinto Aduanero, Pisiga (frontera con Chile)",
-                       "telefono": "N/D",
-                       "coordenadas": {
-                         "latitud": null,
-                         "longitud": null
-                       },
-                       "notas": "Parada fronteriza en Pisiga, utilizada por buses hacia/desde Chile (frontera Colchane)."
-                     }
                    ],
                    "Potosí": [
                      {
                        "nombre": "Terminal de Buses de Potosí (Nueva)",
                        "ciudad": "Potosí",
                        "departamento": "Potosí",
-                       "direccion": "Av. Universitaria (Zona Villa Banzer)",
+                       "direccion": "Zona Central",
                        "telefono": "(2) 6243361",
                        "coordenadas": {
-                         "latitud": null,
-                         "longitud": null
+                         "latitud": -19.557474880606335,
+                         "longitud": -65.76131902615336
                        },
                        "notas": "Terminal principal de Potosí para buses interdepartamentales."
                      },
@@ -117,11 +105,11 @@ export const getBuses = (req, res) => {
                        "nombre": "Terminal Interprovincial de Potosí (Antigua)",
                        "ciudad": "Potosí",
                        "departamento": "Potosí",
-                       "direccion": "Zona Central (antigua terminal)",
+                       "direccion": "Av. Universitaria (Zona Villa Banzer)",
                        "telefono": "N/D",
                        "coordenadas": {
-                         "latitud": null,
-                         "longitud": null
+                         "latitud": -19.578128884575673,
+                         "longitud": -65.7649103822164
                        },
                        "notas": "Antigua terminal de Potosí, actualmente utilizada para transporte interprovincial."
                      },
@@ -132,8 +120,8 @@ export const getBuses = (req, res) => {
                        "direccion": "Zona Norte de Uyuni (Terminal nueva)",
                        "telefono": "N/D",
                        "coordenadas": {
-                         "latitud": null,
-                         "longitud": null
+                         "latitud": -20.46742588472049,
+                         "longitud": -66.82326198140584
                        },
                        "notas": "Terminal terrestre de Uyuni (reciente construcción) para buses interdepartamentales y turísticos."
                      },
@@ -144,10 +132,10 @@ export const getBuses = (req, res) => {
                        "direccion": "Av. Arce, esquina Cabrera (Uyuni)",
                        "telefono": "N/D",
                        "coordenadas": {
-                         "latitud": null,
-                         "longitud": null
+                         "latitud": -20.461011134672614,
+                         "longitud": -66.82610820745816
                        },
-                       "notas": "Parada urbana tradicional en Uyuni (usada antes de la nueva terminal, aún punto de embarque local)."
+                       "notas": "Parada urbana tradicional en Uyuni"
                      },
                      {
                        "nombre": "Terminal de Buses de Villazón",
@@ -156,10 +144,10 @@ export const getBuses = (req, res) => {
                        "direccion": "Zona Central, Villazón",
                        "telefono": "N/D",
                        "coordenadas": {
-                         "latitud": null,
-                         "longitud": null
+                         "latitud": -22.077152491364874,
+                         "longitud": -65.59006756345082
                        },
-                       "notas": "Terminal de Villazón en la frontera con Argentina (La Quiaca)."
+                       "notas": "Terminal de Villazón en la frontera con Argentina (La Quiaca). usada antes de la nueva terminal, aún punto de embarque local"
                      },
                      {
                        "nombre": "Terminal de Buses de Tupiza",
@@ -168,8 +156,8 @@ export const getBuses = (req, res) => {
                        "direccion": "Av. Serrano esquina Suipacha",
                        "telefono": "N/D",
                        "coordenadas": {
-                         "latitud": null,
-                         "longitud": null
+                         "latitud": -21.447528819747955,
+                         "longitud": -65.71671566677665
                        },
                        "notas": "Terminal de buses de Tupiza, con servicios a ciudades de Potosí, Tarija y la frontera argentina."
                      },
@@ -180,8 +168,8 @@ export const getBuses = (req, res) => {
                        "direccion": "Zona Central de Llallagua",
                        "telefono": "N/D",
                        "coordenadas": {
-                         "latitud": null,
-                         "longitud": null
+                         "latitud": -18.41991335873396,
+                         "longitud": -66.58186196877686
                        },
                        "notas": "Terminal de la localidad minera de Llallagua, usada para transporte interprovincial (norte de Potosí)."
                      }
@@ -258,8 +246,8 @@ export const getBuses = (req, res) => {
                        "direccion": "Carretera al Norte s/n, ingreso a Montero",
                        "telefono": "71691702",
                        "coordenadas": {
-                         "latitud": null,
-                         "longitud": null
+                         "latitud": -17.367061294860427,
+                         "longitud": -63.236836958062334
                        },
                        "notas": "Nueva terminal interprovincial en Montero, para buses hacia provincias del norte cruceño."
                      },
@@ -270,8 +258,8 @@ export const getBuses = (req, res) => {
                        "direccion": "Av. Colón s/n, Puerto Quijarro",
                        "telefono": "N/D",
                        "coordenadas": {
-                         "latitud": null,
-                         "longitud": null
+                         "latitud": -19.00724732960371,
+                         "longitud": -57.72930255110495
                        },
                        "notas": "Terminal en Puerto Quijarro, frontera con Brasil (próxima a Corumbá)."
                      },
@@ -282,8 +270,8 @@ export const getBuses = (req, res) => {
                        "direccion": "Zona Estación de Tren, Roboré",
                        "telefono": "N/D",
                        "coordenadas": {
-                         "latitud": null,
-                         "longitud": null
+                         "latitud": -18.347828907538304,
+                         "longitud": -59.751067221745416
                        },
                        "notas": "Parada intermedia en Roboré, ciudad ubicada en la ruta Santa Cruz–Puerto Quijarro."
                      },
@@ -294,8 +282,8 @@ export const getBuses = (req, res) => {
                        "direccion": "Zona central de Samaipata",
                        "telefono": "N/D",
                        "coordenadas": {
-                         "latitud": null,
-                         "longitud": null
+                         "latitud": -17.79839111475814,
+                         "longitud": -63.194030549482726
                        },
                        "notas": "Parada utilizada por buses y trufis hacia Samaipata (destino turístico en la provincia Florida)."
                      },
@@ -306,8 +294,8 @@ export const getBuses = (req, res) => {
                        "direccion": "Av. Velasco s/n",
                        "telefono": "(591) 74953044",
                        "coordenadas": {
-                         "latitud": null,
-                         "longitud": null
+                         "latitud": -16.39316679916418,
+                         "longitud": -60.97015756012253
                        },
                        "notas": "Terminal de San Ignacio de Velasco, para transporte interprovincial e interdepartamental en la Chiquitanía."
                      },
@@ -318,8 +306,8 @@ export const getBuses = (req, res) => {
                        "direccion": "Zona Estación Ferroviaria, San José",
                        "telefono": "N/D",
                        "coordenadas": {
-                         "latitud": null,
-                         "longitud": null
+                         "latitud": -17.83308222837895,
+                         "longitud": -60.7386293325928
                        },
                        "notas": "Parada habitual en San José de Chiquitos, pueblo intermedio en la ruta al Pantanal."
                      },
@@ -330,8 +318,8 @@ export const getBuses = (req, res) => {
                        "direccion": "Zona urbana de San Matías",
                        "telefono": "N/D",
                        "coordenadas": {
-                         "latitud": null,
-                         "longitud": null
+                         "latitud": -16.354427293584955,
+                         "longitud": -58.41149003924832
                        },
                        "notas": "Terminal de la población fronteriza de San Matías (frontera con Brasil)."
                      },
@@ -342,8 +330,8 @@ export const getBuses = (req, res) => {
                        "direccion": "Calle Carlos Daher, Zona La Williams",
                        "telefono": "N/D",
                        "coordenadas": {
-                         "latitud": null,
-                         "longitud": null
+                         "latitud": -20.02021733730353,
+                         "longitud": -63.532036701305955
                        },
                        "notas": "Terminal de Camiri, también conocida como Terminal Isaac Ortuño, para buses hacia Santa Cruz, Sucre y Yacuiba."
                      }
@@ -444,8 +432,8 @@ export const getBuses = (req, res) => {
                        "direccion": "Av. San Martín s/n",
                        "telefono": "(4) 6824794",
                        "coordenadas": {
-                         "latitud": null,
-                         "longitud": null
+                         "latitud": -22.009067492027846,
+                         "longitud": -63.67237910310562
                        },
                        "notas": "Terminal de Yacuiba, ciudad fronteriza con Argentina (Provincia de Salta)."
                      },
@@ -456,8 +444,8 @@ export const getBuses = (req, res) => {
                        "direccion": "Av. Méndez Arcos s/n, Zona Central",
                        "telefono": "72988922",
                        "coordenadas": {
-                         "latitud": null,
-                         "longitud": null
+                         "latitud": -21.267891806476882,
+                         "longitud": -63.457422216617644
                        },
                        "notas": "Terminal de Villamontes (Gran Chaco tarijeño), con servicio a Tarija, Yacuiba y Santa Cruz."
                      },
@@ -468,8 +456,8 @@ export const getBuses = (req, res) => {
                        "direccion": "Zona urbana de Bermejo",
                        "telefono": "67371832",
                        "coordenadas": {
-                         "latitud": null,
-                         "longitud": null
+                         "latitud": -22.74060729770593,
+                         "longitud": -64.33644428870683
                        },
                        "notas": "Terminal de Bermejo, ciudad fronteriza con Argentina (Provincia de Salta)."
                      }
