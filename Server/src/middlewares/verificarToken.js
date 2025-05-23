4effc34c266764a35c0ea60373e82418fc3a13bb
RODRIGOVR <RodrigoVR70@gmail.com>
2025-05-23 16:49:16 -0400

    add-server:reservas endpoint

--- Server/src/middlewares/verificarToken.js
+++ Server/src/middlewares/verificarToken.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
-const SECRET_KEY = process.env.JWT_SECRET || "clave_super_secreta";
+const SECRET_KEY = process.env.JWT_SECRET || "clave_super_segura";
 
 export const verificarToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
